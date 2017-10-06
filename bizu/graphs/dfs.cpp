bba0e5ef6acbb0d828422ccadb5ea4a2bafe3b4e
Sérgio Hilgert Junior <hilgertfacul@gmail.com>
2017-10-06 20:43:02 -0300

    add dfs

--- bizu/graphs/dfs.cpp
+++ bizu/graphs/dfs.cpp
@@ -8,3 +8,14 @@ void dfs(int start, vector<int> graph[], bool visited[]){
     }
   }
 }
+
+// Adjacent Matrix
+void dfs(int start, int graph[][], bool visited[], int graph_size){
+  visited[start] = true;
+  for(int i = 0; i < graph_size; ++i){
+    if(graph[start][i] && !visited[i]){
+      // process the current vertex here
+      dfs(i, graph, visited, graph_size);
+    }
+  }
+}
